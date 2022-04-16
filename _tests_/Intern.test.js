ededd044a621c5b381bf25f9ae8809026303d6e1
NicholasCH <nicholasch96@gmail.com>
2022-04-15 19:03:59 -0500

    tests removed

--- _tests_/Intern.test.js
+++
@@ -1,6 +0,0 @@
-const Intern = require ('../lib/Intern')
-
-test ('Adding a school for an intern', () => {
-    const newIntern = new Intern ('name','manager',1,'email@domain.com','school')
-    expect(newIntern.getSchool()).toBe('school')
-})
